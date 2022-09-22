8f59ccc848fc32e58a31e94aa50bd9debe68e71c
taiysuzu <74975096+taiysuzu@users.noreply.github.com>
2022-09-22 14:15:07 -0400

    Item 2 complete as well

--- Item 2/Source.c
+++ Item 2/Source.c
@@ -5,7 +5,6 @@
 //Item 2: Using a for loop, print a table using ASCII (printable) characters with each line giving an integer, its square root,
 //its square and its cube. Ask the user to enter a lower and upper limit for the table, but limit input to stay between 0 and 50.
 
-
 int main(void)
 {
 	int lowerLimit = 0;
