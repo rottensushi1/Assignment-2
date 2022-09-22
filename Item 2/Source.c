210a11e27f9e9a1f9e477d5795f39c4e22113b48
taiysuzu <74975096+taiysuzu@users.noreply.github.com>
2022-09-22 14:29:07 -0400

    item 2 complete. ( i think )

--- Item 2/Source.c
+++ Item 2/Source.c
@@ -5,27 +5,30 @@
 //Item 2: Using a for loop, print a table using ASCII (printable) characters with each line giving an integer, its square root,
 //its square and its cube. Ask the user to enter a lower and upper limit for the table, but limit input to stay between 0 and 50.
 
+//definitions/constants
+#define LOWERLIMIT   0
+#define UPPERLIMIT   50
+
 int main(void)
 {
-	int lowerLimit = 0;
-	int upperLimit = 50;																//initial limit values
+	int lowerLimitInput = 0, upperLimitInput = 50;										//initial limit values same as constant limits
 
-	printf("Please enter lower limit (between 0-49): ");								//prompt user for first input
+	printf("Please enter lower limit (between 0-50): ");								//prompt user for first input
 
-	scanf_s("%d", &lowerLimit);															//scan and save lower limit
+	scanf_s("%d", &lowerLimitInput);													//scan and save lower limit
 
-	if (0 <= lowerLimit && lowerLimit <= 50)											//check if limit is between 0 & 50
+	if (LOWERLIMIT <= lowerLimitInput && lowerLimitInput <= UPPERLIMIT)					//check if limit is between 0 & 50
 	{
-		printf("Please enter upper limit (between %d-50): ", lowerLimit);				//prompt user for second input
+		printf("Please enter upper limit (between %d-50): ", lowerLimitInput);			//prompt user for second input
 
-		scanf_s("%d", &upperLimit);														//scan and save upper limit
+		scanf_s("%d", &upperLimitInput);												//scan and save upper limit
 
-		if (upperLimit <= 50 && lowerLimit <= upperLimit)								//check if limit is between lower limit & 50
+		if (upperLimitInput <= UPPERLIMIT && lowerLimitInput <= upperLimitInput)			//check if limit is between lower limit & 50
 		{
 			printf("-----------------------------\n");
 			printf("x -- sqrt(x) -- x^2 -- x^3\n");										//print top of table
 
-			for (int i = lowerLimit; i <= upperLimit; i++)								//caculate and print output with lines for table in between
+			for (int i = lowerLimitInput; i <= upperLimitInput; i++)					//caculate and print output with lines for table in between
 			{
 				double iDouble = i, iSquareRoot = sqrt(iDouble);
 				int iSquare = i * i, iCube = pow(i, 3);
