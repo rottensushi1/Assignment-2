211af168e850641a4100a6ff1e9c754992e137e5
taiyo <tsuzuki7660@conestogac.on.ca>
2022-09-29 13:30:00 -0400

    note

--- Item 1/Source.c
+++ Item 1/Source.c
@@ -6,6 +6,8 @@
 //Item 1: Take an integer input and then print all the integers from (and including) that value
 //up to (and including) a value larger by 11, separating each output value by a newline
 
+//todo: add a function to replace input checking with if statements.
+
 //definitions
 #define LARGER_BY_ELEVEN 11
 
